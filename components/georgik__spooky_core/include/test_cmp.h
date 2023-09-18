31b1fed10233a98275e9bb82fa64b756fed62772
Juraj Michalek <juraj.michalek@espressif.com>
2023-09-18 16:20:39 +0200

    acquire value from Rust allocated buffer

--- components/georgik__spooky_core/include/test_cmp.h
+++ components/georgik__spooky_core/include/test_cmp.h
@@ -1,2 +1,19 @@
 const char* hello();
 
+#include <stdint.h>
+#include <stddef.h> // for NULL and size_t
+
+typedef struct {
+    uint16_t value;
+} Rgb565;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Function to get a pointer to the framebuffer data
+const void* framebuffer();
+
+#ifdef __cplusplus
+}
+#endif
--- components/georgik__spooky_core/rust_hello/src/lib.rs
+++ components/georgik__spooky_core/rust_hello/src/lib.rs
@@ -1,4 +1,17 @@
-#![no_std]
+#![cfg_attr(not(feature = "std"), no_std)]
+pub mod assets;
+pub mod demo_movement_controller;
+pub mod engine;
+pub mod maze;
+pub mod movement_controller;
+pub mod nomovement_controller;
+pub mod spritebuf;
+pub mod universe;
+
+use embedded_graphics::{pixelcolor::Rgb565, prelude::DrawTarget};
+use embedded_graphics_framebuf::FrameBuf;
+use embedded_graphics::prelude::RgbColor;
+
 
 use core::ffi::c_void;
 use core::panic::PanicInfo;
@@ -10,10 +23,18 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 // Define a null-terminated string as a byte array
-static HELLO_ESP32: &'static [u8] = b"Hello ESP64\0";
+static HELLO_ESP32: &'static [u8] = b"Hello Spooky\0";
 
 #[no_mangle]
 pub extern "C" fn hello() -> *const c_void {
     HELLO_ESP32.as_ptr() as *const c_void
 }
 
+static mut framebuffer_data: [Rgb565; 240 * 240 * 2] = [Rgb565::WHITE ; 240 * 240 * 2];
+
+#[no_mangle]
+pub extern "C" fn framebuffer() -> *const c_void {
+    unsafe {
+        framebuffer_data.as_ptr() as *const c_void
+    }
+}
